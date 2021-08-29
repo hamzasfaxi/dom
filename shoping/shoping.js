b25556e9432d3373fa81b8bcdd8b4e348e05d147
hamzasfaxi <88566062+hamzasfaxi@users.noreply.github.com>
2021-08-29 18:11:42 +0100

    Add files via upload

--- shoping/shoping.js
+++ shoping/shoping.js
@@ -1,1 +1,14 @@
-document.querySelector(".button1").setAttribute("enabled")
+  var i = 0;
+    function buttonplus() {
+        document.getElementById('but1').value = ++i;
+        }
+
+
+
+         var i = 0;
+    function buttonminus() {
+        document.getElementById('but2').value = --i;
+        }
+
+        
+   

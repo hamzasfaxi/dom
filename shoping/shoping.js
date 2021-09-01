21a97710e252fc6918165fb72909585149997133
hamzasfaxi <88566062+hamzasfaxi@users.noreply.github.com>
2021-09-01 11:35:39 +0100

    Add files via upload

--- shoping/shoping.js
+++ shoping/shoping.js
@@ -3,12 +3,56 @@
         document.getElementById('but1').value = ++i;
         }
 
+        var i = 0;
+    function buttonplus1() {
+        document.getElementById('but2').value = ++i;
+        }
+
+        var i = 0;
+    function buttonplus2() {
+        document.getElementById('but3').value = ++i;
+        }
+
+
 
 
          var i = 0;
     function buttonminus() {
-        document.getElementById('but2').value = --i;
+      if(document.getElementById('but1').value>0){
+        document.getElementById('but1').value = --i;}
         }
 
         
-   
+         var i = 0;
+    function buttonminus1() {
+      if(document.getElementById('but2').value>0){
+        document.getElementById('but2').value = --i;}
+        }
+
+        
+         var i = 0;
+    function buttonminus2() {
+      if(document.getElementById('but3').value>0){
+        document.getElementById('but3').value = --i;}
+        }
+
+
+        document.querySelector('#delet').addEventListener(('click'),function(){
+  document.getElementById('tablerow1').remove()
+})
+
+ document.querySelector('#delet2').addEventListener(('click'),function(){
+  document.getElementById('tablerow2').remove()
+})
+
+ document.querySelector('#delet3').addEventListener(('click'),function(){
+  document.getElementById('tablerow3').remove()
+})
+
+
+
+ var i = 0;
+   function MULTIPLY (price, quantity) {
+    return price*quantity;
+   }
+  
